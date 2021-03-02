b241609efc29880745066ca00ff7f9269e01daad
varadizsolt <varadizsolt0212@gmail.com>
2021-03-02 15:09:14 +0100

    Kiindulo helyzet

---
+++ src/lengyelforma/LengyelForma.java
@@ -0,0 +1,13 @@
+package lengyelforma;
+public class LengyelForma {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+        
+        
+        
+    }
+    
+}
